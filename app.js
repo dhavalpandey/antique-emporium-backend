4e3d22fb4b7d31be516cada4a9615f96ddfda33b
Antique-Emporium <antique.emporium2021@gmail.com>
2021-03-09 14:27:27 +0000

    graphql

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -15,16 +17,33 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const rateLimit = require("express-rate-limit");
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
-
+const apollo_server_express_1 = require("apollo-server-express");
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
+const typeDefs = apollo_server_express_1.gql `
+  type Query {
+    hello: String!
+  }
+`;
+const resolvers = {
+    Query: {
+        hello: () => "hello"
+    }
+};
+const server = new apollo_server_express_1.ApolloServer({
+    typeDefs,
+    resolvers
+});
+server.applyMiddleware({ app });
 let limiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 10
+    max: 10,
 });
 app.use(cors({
-    origin: 'https://antique-emporium.netlify.app'
-  }));
+    origin: 'https://antique-emporium.netlify.app',
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -32,7 +51,7 @@ app.use(limiter);
 app.use(session({
     secret: SECRET,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -51,7 +70,7 @@ app.get('/status', (req, res) => {
         res.json({ message: 'you are logged in!' });
     }
     else {
-        res.status(401).json({ message: "you must log in" });
+        res.status(401).json({ message: 'you must log in' });
     }
 });
 app.listen(PORT, console.log(`Server running at port ${PORT}`));
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
 }
 
 const passport: any = require('passport');
@@ -10,40 +10,67 @@ const flash = require('connect-flash');
 require('./config/passport')(passport);
 const json = require('./data/products');
 const connectDB = require('./config/db');
-const productRoutes = require('./routes/productRoutes')
+const productRoutes = require('./routes/productRoutes');
 connectDB();
 
+//enviorment vars
 const PORT = process.env.PORT || 5000;
 const SECRET = process.env.SECRET;
 
+//imports
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser  = require('body-parser');
-const rateLimit = require("express-rate-limit");
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
-const redis = require('redis')
-const client = redis.createClient()
-const cache = require('memory-cache');
+
+//GraphQL
+import { ApolloServer, gql } from 'apollo-server-express';
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => "hello"
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app });
 
 let limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 10
+    windowMs: 1 * 60 * 1000,
+    max: 10,
 });
-app.use(cors({
-  origin: 'https://antique-emporium.netlify.app'
-}));
+
+app.use(
+    cors({
+        origin: 'https://antique-emporium.netlify.app',
+    })
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(limiter);
 
 app.use(
-  session({
-    secret: SECRET,
-    resave: true,
-    saveUninitialized: true
-  })
+    session({
+        secret: SECRET,
+        resave: true,
+        saveUninitialized: true,
+    })
 );
 
 // Passport middleware
@@ -54,22 +81,22 @@ app.use(passport.session());
 app.use(flash());
 
 app.use((req: any, res: any, next: any) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
 });
 
 app.use('/api', productRoutes);
-app.use('/' , require('./routes/registrationRoutes.js'));
-app.use('/' , require('./routes/loginRoutes.js'));
+app.use('/', require('./routes/registrationRoutes.js'));
+app.use('/', require('./routes/loginRoutes.js'));
 
 app.get('/status', (req: any, res: any) => {
-  if(req.isAuthenticated()) {
-    res.json({message: 'you are logged in!'})
-  } else {
-    res.status(401).json({message: "you must log in"})
-  }
-})
+    if (req.isAuthenticated()) {
+        res.json({ message: 'you are logged in!' });
+    } else {
+        res.status(401).json({ message: 'you must log in' });
+    }
+});
 
-app.listen(PORT, console.log(`Server running at port ${PORT}`))
+app.listen(PORT, console.log(`Server running at port ${PORT}`));
